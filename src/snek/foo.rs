3b268b817fec7ec315d328f332ad04b4df7bc8d7
Nathan Shelly <nathan.shelly@opendoor.com>
2019-11-05 20:59:34 -0800

    Add failing test

--- src/snek/foo.rs
+++ src/snek/foo.rs
@@ -1,5 +1,14 @@
 use tui;
 
 pub fn foo() {
-    println!("Hello world!");
+  println!("Hello world!");
+}
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn panic() {
+    foo();
+    panic!();
+  }
 }
