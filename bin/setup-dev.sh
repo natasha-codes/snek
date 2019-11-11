25c20d27be0365a02d06598d00bb7014785ac17f
Nathan Shelly <nathan.shelly@opendoor.com>
2019-11-10 21:42:38 -0800

    Fix shellcheck lints

--- bin/setup-dev.sh
+++ bin/setup-dev.sh
@@ -1,9 +1,9 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 # Symlink pre-commit hooks
-pushd ../.git/hooks
+pushd ../.git/hooks || exit
 ln -s ../../.githooks/pre-commit pre-commit
-popd
+popd || exit
 
 # Install [clippy](https://github.com/rust-lang/rust-clippy)
 rustup component add clippy
