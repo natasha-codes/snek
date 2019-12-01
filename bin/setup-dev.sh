402afa00b0bc21397787aa59d1bd4b626bd2a285
Nathan Shelly <nathan.shelly@opendoor.com>
2019-11-30 21:01:26 -0800

    Force pre-commit hook symlink

--- bin/setup-dev.sh
+++ bin/setup-dev.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Symlink pre-commit hooks
 root=$(git rev-parse --show-toplevel)
 pushd "$root/.git/hooks"
-ln -s ../../.githooks/pre-commit pre-commit
+ln -sf ../../.githooks/pre-commit pre-commit
 popd
 
 # Install [clippy](https://github.com/rust-lang/rust-clippy), which only works on nightly
