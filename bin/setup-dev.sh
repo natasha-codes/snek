072595511ee4a3df551440b73f1951a1bd3f51b9
Nathan Shelly <nsnshelly@gmail.com>
2019-12-10 22:20:55 -0800

    Guard pre-commit symlink & shellcheck installation (#27)
    * Force pre-commit hook symlink
    
    * Conditionally install shellcheck

--- bin/setup-dev.sh
+++ bin/setup-dev.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Symlink pre-commit hooks
 root=$(git rev-parse --show-toplevel)
 pushd "$root/.git/hooks"
-ln -s ../../.githooks/pre-commit pre-commit
+ln -sf ../../.githooks/pre-commit pre-commit
 popd
 
 # Install [clippy](https://github.com/rust-lang/rust-clippy), which only works on nightly
@@ -13,4 +13,4 @@ rustup toolchain add nightly
 rustup component add clippy --toolchain nightly
 
 # [shellcheck](https://github.com/koalaman/shellcheck)
-brew install shellcheck
+command -v shellcheck > /dev/null || brew install shellcheck
