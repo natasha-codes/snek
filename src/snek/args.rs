1f4170b673a9074a93ae4b7b49b06eab48181f53
Nathan Shelly <nsnshelly@gmail.com>
2020-02-23 21:42:17 -0800

    Remove unneeded args.rs

--- src/snek/args.rs
+++
@@ -1,8 +0,0 @@
-/// Play the classic game snake! 🐍
-#[derive(Clap, Debug)]
-#[clap(version = "0.1", author = "Nathan Shelly & Sasha Weiss")]
-pub struct Args {
-  /// Sets the amount of food on the board at any given time
-  #[clap(long = "food-count")]
-  food_count: Option<i32>,
-}
